e2d64cbbbe0a3a7361ca0a168a19a038c70291a3
oss <po0858@naver.com>
2024-03-21 18:00:02 +0900

    랜딩페이지 반응형 추가

--- js/common.js
+++ js/common.js
@@ -1,9 +1,22 @@
 $("#fullpage").fullpage({
-  anchors: ["anchor1", "anchor2", "anchor3", "anchor4"],
+  anchors: ["anchor1", "anchor2", "anchor3", "anchor4", "anchor5"],
   menu: "#menu",
+  // responsiveHeight: 850,
+  responsiveWidth: 768,
+  scrollOverflow: true,
 });
-let ht = $("header").outerHeight(true);
-$(".wrap").css({ paddingTop: ht });
+
+function checkWindowSize() {
+  if (window.matchMedia("(max-width: 480px)").matches) {
+    $(".wrap").css({ paddingTop: 0 });
+  } else {
+    let ht = $("header").outerHeight(true);
+    $(".wrap").css({ paddingTop: ht });
+  }
+}
+checkWindowSize();
+window.addEventListener("resize", checkWindowSize);
+
 $(".gnb li").hover(
   function () {
     $(".gnb a").css({ opacity: "0.6" });
